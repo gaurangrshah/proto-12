de4ebca92227e6a7dd83ae31ea2bb9e1b1f5ebe9
gshah2020 <gaurang.r.shah@gmail.com>
2023-05-13 17:48:29 -0400

    add types

---
+++ src/types/analytics.d.ts
@@ -0,0 +1,8 @@
+declare module '@analytics/mixpanel';
+
+declare module '@analytics/cookie-utils';
+
+declare module '@analytics/activity-utils';
+declare module 'analytics-plugin-do-not-track' {
+  export default function doNotTrackPlugin(): void;
+}
---
+++ src/types/proprietary.ts
@@ -0,0 +1,48 @@
+import type { FC } from 'react';
+import type { NextComponentType, NextPage } from 'next';
+
+export type WithSEO = {
+  title?: string;
+  description?: string;
+  image?: string;
+  url?: string;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type FCwChildren<T = Record<string, any> | undefined> = FC<
+  { children: React.ReactNode } & Partial<T>
+>;
+
+export type NextPageWithAuth = NextPage & { auth: boolean };
+export type NextComponentTypeWithAuth = NextComponentType & { auth?: boolean };
+
+/* -------------------------------------------------------------------------- */
+/*                                    Misc.                                    */
+/* -------------------------------------------------------------------------- */
+
+export type DeepPartial<T> = T extends object
+  ? { [P in keyof T]?: DeepPartial<T[P]> }
+  : T;
+
+export type AnyPrimitive = string | number | boolean;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AnyFunction = (...args: any[]) => any;
+
+export type ExtendProp<Obj, Prop, NewValue> = {
+  [P in keyof Obj]: P extends Prop ? Obj[P] & NewValue : Obj[P];
+};
+
+export declare type Awaitable<T> = T | PromiseLike<T>;
+
+export type Maybe = {
+  string: string | null | undefined;
+  number: string | null | undefined;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  object: Record<string, any> | null | undefined;
+};
+
+export interface StringObj {
+  [p: string]: {
+    [p: string]: string;
+  };
+}
