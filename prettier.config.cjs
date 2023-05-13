b7a7a142d3aa5ade6aed064cbc9329aff9bd13d7
gshah2020 <gaurang.r.shah@gmail.com>
2023-05-13 01:42:43 -0400

    chore: add prettier + .dotfiles update

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  pluginSearchDirs: false // disable auto-loading of plugins becaues of priority loading of plugins.
 };
 
 module.exports = config;
